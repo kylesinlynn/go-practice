d8acec85db7acfc23b682763960cb3151c297b4b
Kyle Sin Lynn <ks.lynn@mit.com.mm>
2024-09-24 11:26:03 +0630

    Completed lesson 4: Functions in Go

--- main.go
+++ main.go
@@ -2,60 +2,33 @@ package main
 
 import "fmt"
 
+// Function that take two integers and returns their sum
+func add(a int, b int) int {
+	return a + b
+}
+
+// Function that returns the sum and difference of two integers
+func sumAndDifference(a int, b int) (int, int) {
+	return a + b, a - b
+}
+
+// Function with named return values
+func namedReturns(a int, b int) (sum int, difference int) {
+	sum = a + b
+	difference = a - b
+	return // Returns named return values
+}
+
 func main() {
-	// If-Else Statement
-	age := 18
+	// Calling the add function
+	result := add(5, 7)
+	fmt.Println("The sum is:", result)
 
-	if age < 18 {
-		fmt.Println("You are a minor.")
-	} else if age >= 18 && age < 65 {
-		fmt.Println("You are an adult.")
-	} else {
-		fmt.Println("You are a senior.")
-	}
+	// Calling sumAndDifference function
+	sum, diff := sumAndDifference(3, 5)
+	fmt.Println("Sum:", sum, ", Difference:", diff)
 
-	fmt.Println() // For better readability in output
-
-	// Switch Statement
-	day := 3
-
-	switch day {
-	case 1:
-		fmt.Println("Monday")
-	case 2:
-		fmt.Println("Tuesday")
-	case 3:
-		fmt.Println("Wednesday")
-	case 4:
-		fmt.Println("Thursday")
-	case 5:
-		fmt.Println("Friday")
-	}
-
-	fmt.Println() // For better readability in output
-
-	// For Loop
-	// Basic for loop
-	fmt.Println("Basic for loop:")
-	for i := 0; i < 5; i++ {
-		fmt.Println("Iteration:", i)
-	}
-
-	fmt.Println() // For better readability in output
-
-	// For loop with a condition
-	j := 0
-	fmt.Println("For loop with a condition:")
-	for j < 3 {
-		fmt.Println("Iteration:", j)
-		j++
-	}
-
-	// Infinite loop (be careful!)
-	// Uncomment the next lines to see it in action
-	// fmt.Println("This will run forever!")
-	// for {
-	//     fmt.Println("This will run forever!")
-	// }
-
+	// Calling namedReturns function
+	sumNamed, diffNamed := namedReturns(4, 2)
+	fmt.Println("Sum:", sumNamed, ", Difference:", diffNamed)
 }
