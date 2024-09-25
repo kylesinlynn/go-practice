92eecc1caf26da9612aaafdd5e6848c6e1c7815f
Kyle Sin Lynn <kylesinlynn@gmail.com>
2024-09-25 11:13:49 +0630

    Completed lesson 10: Static File Server in Go

--- main.go
+++ main.go
@@ -8,13 +8,31 @@ import (
 
 // Handler function for the root route "/"
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Go Web Server!")
+	fmt.Fprintf(w, `
+		<html>
+			<head>
+				<link rel="stylesheet" href="/static/style.css">
+			</head>
+			<body>
+				<h1>Welcome to the Go Web Server!</h1>
+			</body>
+		</html>
+	`)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the about page")
 }
 
 // Start the server
 func main() {
+	// Serve the static files from the "static" directory
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	// Define the route and corresponding handler function
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
 
 	// Start the HTTP server
 	log.Println("Starting server on: 8080")
